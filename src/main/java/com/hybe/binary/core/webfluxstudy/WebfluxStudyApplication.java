75b4781d29bb0aa473df74f05e1e50f10dc3594f
ybh <ybh@binarycorp.com>
2024-09-28 19:17:43 +0900

    웹플럭스 스터디 프로젝트 생성

---
+++ src/main/java/com/hybe/binary/core/webfluxstudy/WebfluxStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hybe.binary.core.webfluxstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hybe/binary/core/webfluxstudy/WebfluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hybe.binary.core.webfluxstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
